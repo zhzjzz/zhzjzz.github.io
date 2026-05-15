834a3d6bcca178cecb8d229a8856b09a5165074d
bbbbbbbbbbb114 <staygold00@qq.com>
2026-05-15 20:01:25 +0800

    feat: add itinerary planner preview contract

---
+++ data-structure-design-backend/src/main/java/com/travel/system/dto/ItineraryPlannerPreviewRequest.java
@@ -0,0 +1,31 @@
+package com.travel.system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItineraryPlannerPreviewRequest {
+    private LocalDateTime departureTime;
+    private String strategy;
+    private Boolean optimizeVisitOrder;
+    private List<PlannerSpot> spots;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PlannerSpot {
+        private Long spotId;
+        private Long destinationId;
+        private String spotName;
+        private Double latitude;
+        private Double longitude;
+        private String transportMode;
+        private Boolean selected;
+    }
+}
